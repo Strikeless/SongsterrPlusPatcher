fd38816e651762d64808983e7701f6d666ab6c3f
Strikeless <56199495+Strikeless@users.noreply.github.com>
2026-08-25 15:28:48 +0300

    Initial version of new patch
    Updated script to work again with recent changes to the site. Verified to work on Firefox+Violentmonkey. This is once again nearly a full rewrite, so issues are expected until those are ironed out.
    
    With this update, the script is now licensed under the MIT license, just to have a little ground to stand on if someone publishes a really sketchy derivative. You are still free to create and publish derivatives, just please don't release malware.
    
    Version bumped to 1.3.0

--- user.js
+++ user.js
@@ -1,132 +1,149 @@
 // ==UserScript==
 // @name Songsterr Plus Patcher
 // @namespace https://github.com/Strikeless
-// @version 1.2.3-PATCHED
+// @version 1.3.0
 // @description Trick Songsterr to unlock plus features.
-// @license The Unlicense
+// @license MIT
 // @supportURL https://github.com/Strikeless/SongsterrPlusPatcher
 // @match http*://*.songsterr.com/*
 // @run-at document-start
 // @grant unsafeWindow
 // ==/UserScript==
 
+/*
+Copyright 2026, https://github.com/Strikeless
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 (function () {
-    console.log("Songsterr Plus Patcher: Patched as of 2026-08-24. This version does nothing to avoid your account possibly getting flagged. Working on a fix, script should be updated soon.");
-    /*
-    function notifyError(err) {
-        alert(
-            "Songsterr Plus Patcher encountered an error."
-            + "\nfeel free to report this issue at https://github.com/Strikeless/SongsterrPlusPatcher ."
-            + "\nIf the issue persists, you can disable the userscript and try some of the other userscripts for Songsterr on Greasyfork."
-            + "\n    (alternatively, consider subscribing Songsterr plus, if you have the money to throw and you enjoy their service.)"
-            + "\n\n" + err
-        );
-    }
+    'use strict';
 
-    try {
-        const fetchParent = unsafeWindow || window; // unsafeWindow required to wrap the fetch function in the same context that the actual site uses.
-        const innerFetch = fetchParent.fetch;
+    // unsafeWindow refers to "the original window object of the webpage that allows reading or modifying global variables",
+    // which we must use if we want to modify things when running in a userscript manager with sandboxing (probably all the major ones?).
+    // https://violentmonkey.github.io/api/gm/#unsafewindow
+    const win = unsafeWindow || window;
 
-        function mockProfile(profile) {
-            if (profile.plan == "plus") {
-                console.log("Songsterr Plus Patcher: You already have Songsterr plus!");
-                return profile;
-            }
-
-            profile.plan = "plus";
-            profile.subscription = {
-                plan: {
-                    id: "plus"
-                }
-            };
-
-            return profile;
-        }
-
+    function patchStateData(state) {
         /*
-         * Wrap the fetch function in our own version that intercepts requests to the profile detail endpoint, mocking plus status.
-         * /
-        function interceptingFetch(resource, options) {
-            var resource_url = JSON.stringify(resource); // Not really sure if JSON.stringify is the right tool for the job, but it works. (unlike toString)
+         * Fake demo mode for plus features.
+         * This has become easier than faking a plus profile, which would now require nulling a signature check (with asymmetric keys) and a bunch of request spoofing.
+         */
+        state.demo = {
+            active: true,
+            enabled: true
+        };
+        /*
+        state.query = {
+            demo: "enabled"
+        }
+        state.queryContent = {
+            demo: "enabled"
+        }
+        */
 
-            if (resource_url.includes("/auth/profile")) {
-                console.log("Songsterr Plus Patcher: Intercepting /auth/profile request to " + resource_url + ".");
+        return state;
+    }
 
-                return innerFetch(resource, options)
-                    .then(response => response.json())
-                    .then(responseProfile => mockProfile(responseProfile))
-                    .then(mockedProfile => new Response(JSON.stringify(mockedProfile)))
-                    .catch(err => notifyError(err));
-            } else {
-                return innerFetch(resource, options);
-            }
+    let appMutationObserverApplied = false;
+    /// Whenever the tab viewer is switched by the site, it gets reset to the non-demo version.
+    /// This function starts an observer that will try to detect that and reload the site to get demo mode again.
+    // TODO: This is stupid, the reloads are annoying. Would be great if we figured out how to get the demo mode to stay without a full page reload.
+    function applyAppMutationObserver() {
+        if (appMutationObserverApplied) return;
+        appMutationObserverApplied = true;
+
+        const appElement = document.getElementById("app");
+        if (appElement == null) {
+            console.log("SongsterrPlusPatcher: Didn't find app element, manual reloads will be necessary.");
+            return;
         }
 
-        Object.defineProperty(fetchParent, "fetch", {
-            value: function () {
-                return interceptingFetch(...arguments);
-            },
-            configurable: true,
-            enumerable: false,
-            writable: true,
-        });
+        const appElementObserverCallback = (mutationList, observer) => {
+            for (const mutation of mutationList) {
+                if (mutation.type != "childList") continue;
 
-        window.addEventListener("DOMContentLoaded", () => {
-            try {
-                /*
-                 * Additionally change user.hasPlus to true and user.profile.plan to "plus" in the state JSON.
-                 * /
-                const stateElement = document.getElementById("state");
-                const stateJson = JSON.parse(stateElement.innerHTML);
+                let lockIconElements = document.getElementsByClassName("_8e144G_lock");
+                if (lockIconElements.length == 0) continue;
 
-                stateJson.user.hasPlus = true;
-                if (stateJson.user.profile != null) {
-                    stateJson.user.profile.plan = "plus";
-                } else {
-                    // Logged out user, faking a whole profile here to circumvent problems.
-                    stateJson.user.profile = {
-                        id: 100000000,
-                        uid: 100000000,
-                        email: "fakeforplus@example.com",
-                        name: "fakeforplus",
-                        plan: "plus",
-                        permissions: [],
-                        subscription: null,
-                        sra_license: "none",
-                        bonus: {
-                            activeStart: null,
-                            activeEnd: null,
-                            balance: 0,
-                            balanceMinutes: 0
-                        },
-                        bonusPurchasedFeatures: [],
-                        signature: "invalid_signature_with_no_purpose_other_than_to_exist",
-                        created_at: "2025-00-00T00:00:00.000Z",
-                        last_signin_date: "2025-00-00T00:00:00.000Z",
-                        hadPlusBeforeSE: false,
-                        password_change_required: false,
-                        preferencesNotifications: {
-                            notificationsEmails: false,
-                            researchEmails: false
-                        }
-                    };
-                }
-                stateElement.innerHTML = JSON.stringify(stateJson);
-
-                /*
-                 * For some reason when reloading or opening a tab directly via URL, the tab viewer doesn't load the actual tablature.
-                 * This attempts to fix the issue by removing the parent apptab element, hopefully resulting in the site recreating it with the tablature.
-                 * /
-                if (document.getElementById("tablature") == null) {
-                    console.log("Songsterr Plus Patcher: tablature element doesn't exist, attempting to fix by removing entire apptab element and letting site recreate it.");
-                    document.getElementById("apptab").remove();
-                }
-            } catch (err) {
-                notifyError(err);
+                console.log("SongsterrPlusPatcher: Lost demo mode, reloading.");
+                window.location.reload();
             }
-        });
-    } catch (err) {
-        notifyError(err);
+        };
+        new MutationObserver(appElementObserverCallback).observe(appElement, { childList: true });
     }
-    */
+
+    function appClientEarlyHook() {
+        console.log("SongsterrPlusPatcher: Running appClient early hook");
+
+        const stateJsonElement = document.getElementById("state");
+        const stateData = JSON.parse(stateJsonElement.innerHTML);
+        const stateDataPatched = patchStateData(stateData);
+        stateJsonElement.innerHTML = JSON.stringify(stateDataPatched);
+
+        // The apptab has already been populated with buttons for free users, so remove this "stale" version.
+        // The site should create the apptab again, now with patched state.
+        document.getElementById("apptab").remove();
+
+        // Initial patching done, but we still have some UI fixing to do once the new apptab is ready.
+        // Observe for the added apptab, and run the late hook once the new apptab has been added.
+        const apptabAddedObserverCallback = (mutationList, observer) => {
+            for (const mutation of mutationList) {
+                if (mutation.type != "childList") continue;
+
+                for (const addedChildNode of mutation.addedNodes) {
+                    if (addedChildNode.id != "apptab") continue;
+                    observer.disconnect();
+                    apptabLateHook();
+                }
+            }
+        };
+        const apptabParentElement = document.getElementById("app");
+        new MutationObserver(apptabAddedObserverCallback).observe(apptabParentElement, { childList: true });
+
+        applyAppMutationObserver();
+    }
+
+    function apptabLateHook() {
+        console.log("SongsterrPlusPatcher: Running apptab late hook");
+
+        // Since the site thinks we're in demo mode, some links have ?demo=enabled appended to them.
+        // We don't need that nor do we really want to confuse the server with demo mode when it disagrees.
+        const demoLinkElements = document.querySelectorAll("a[href*='?demo=']");
+        for (const demoLinkElement of demoLinkElements) {
+            demoLinkElement.outerHTML = demoLinkElement.outerHTML
+                .replaceAll("?demo=enabled", "")
+                .replaceAll("?demo=disabled", "");
+        }
+
+        const demoSongMarkerElement = document.querySelector("a[class*='_demo']");
+        if (demoSongMarkerElement != null) demoSongMarkerElement.remove();
+
+        const topBarPlusButtonElement = document.querySelector("div:has(> #menu-plus)");
+        if (topBarPlusButtonElement != null) topBarPlusButtonElement.remove();
+
+        const headerRegenerateElement = document.getElementById("header-regenerate");
+        if (headerRegenerateElement != null) headerRegenerateElement.remove();
+    }
+
+    // The site's appClient script reads __APP_INITIALIZED very early on (before it has read state or anything like that).
+    // Hook a getter in front of that variable, where we will run our early patching code, before appClient gets a chance to do anything meaningful.
+    let appInitializedValue = false;
+    Object.defineProperty(
+        win,
+        "__APP_INITIALISED",
+        {
+            get() {
+                appClientEarlyHook();
+                return appInitializedValue;
+            },
+            set(value) {
+                appInitializedValue = value;
+            }
+        }
+    );
 })();
